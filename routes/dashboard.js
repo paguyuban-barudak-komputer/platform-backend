7e3a9bef5623ec96e51e7fca54f84843c05db157
fikriks <fikriks130@gmail.com>
2023-07-28 21:56:40 +0700

    add: middleware :rocket:

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
+const { isLoginAdmin } = require('../middleware/authMiddleware')
+
+router.use(isLoginAdmin)
 
 router.get("/", dashboardController.index);
 
