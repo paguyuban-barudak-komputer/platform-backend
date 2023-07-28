7be990f524b8e348c87b96f20e5cfa9fdbc29b33
fikriks <fikriks130@gmail.com>
2023-07-29 00:36:43 +0700

    add: posts :rocket:

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const loginRouter = require('./routes/login');
 const dashboardRouter = require('./routes/dashboard');
 const categoriesRouter = require('./routes/categories');
 const tagsRouter = require('./routes/tags');
+const postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -47,6 +48,7 @@ app.use('/dashboard', dashboardRouter);
 app.use('/categories', categoriesRouter);
 app.use('/tags', tagsRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/posts.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const postsController = require("../controllers/postsController");
+const multer = require('multer')
+const os = require('os')
+
+router.get("/", postsController.index);
+router.get("/create", postsController.create);
+router.get("/:id/edit", postsController.edit);
+
+router.post("/store", multer({ dest: os.tmpdir() }).single('thumbnail'), postsController.store);
+
+router.put("/:id/update", multer({ dest: os.tmpdir() }).single('thumbnail'), postsController.update);
+router.delete("/:id", postsController.destroy);
+
+module.exports = router;
